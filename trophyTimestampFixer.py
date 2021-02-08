63096b11fe9624280aac19595efce1e2c24c2093
Kemal SANLI <kemalsanli@outlook.com.tr>
2021-02-09 01:25:07 +0300

    much better now

--- trophyTimestampFixer.py
+++ trophyTimestampFixer.py
@@ -44,7 +44,7 @@ print("                                           github.com/kemalsanli")
 
 
 def fixer(path):
-    
+
     try:
         sqliteConnection = sqlite3.connect(path)
         cursor = sqliteConnection.cursor()
@@ -53,22 +53,22 @@ def fixer(path):
 
         sqlite_update_query = """UPDATE tbl_trophy_title_entry SET time_last_update=time_last_update_uc where time_last_update=='0001-01-01T00:00:00.00Z'"""
         cursor.execute(sqlite_update_query)
-        print("First Querry OK {} row affected.".format(cursor.rowcount))
+        print("First Query OK {} row affected.".format(cursor.rowcount))
         totalRow=cursor.rowcount
         sqlite_update_query = """UPDATE tbl_trophy_title SET time_last_update=time_last_update_uc, time_last_unlocked=time_last_update_uc WHERE time_last_update == '0001-01-01T00:00:00.00Z' or time_last_unlocked == '0001-01-01T00:00:00.00Z'"""
         cursor.execute(sqlite_update_query)
-        print("Second Querry OK {} row affected.".format(cursor.rowcount))
+        print("Second Query OK {} row affected.".format(cursor.rowcount))
         totalRow=totalRow+cursor.rowcount
         sqlite_update_query = """UPDATE tbl_trophy_flag SET time_unlocked=time_unlocked_uc where time_unlocked=='0001-01-01T00:00:00.00Z'"""
         cursor.execute(sqlite_update_query)
-        print("Third Querry OK {} row affected.\n".format(cursor.rowcount))
+        print("Third Query OK {} row affected.\n".format(cursor.rowcount))
         totalRow=totalRow+cursor.rowcount
         sqliteConnection.commit()
         print("{} rows updated successfully \n".format(totalRow))
         sqliteConnection.commit()
         cursor.close()
         totalRow=0
-        
+
 
     except sqlite3.Error as error:
         print("Failed to update sqlite table \n", error)
